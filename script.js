9c9c5c298b70b0cb3dfbcf70d40378b1dffbe180
Seniru Pasan Indira <34127015+Seniru@users.noreply.github.com>
2019-04-21 17:50:32 +0530

    Added keyboard handler functions *Added keyboard handler functions so that expressions can inputted via the keyboard.

--- script.js
+++ script.js
@@ -1,85 +1,10 @@
 let input;
 let exp;
-let openedBraces = 0;
 let inversed = false;
 let expCont = '';
 let temp = '';
 let memory = [];
 let hist;
-
-// unpacking Math object
-const {PI, sqrt, cbrt, sin, cos, tan, asin, acos, atan, log10, abs} = Math;
-
-// dictionary for holding special symbols
-const sym = {
-  π: 'PI',
-  '√': 'sqrt',
-  '∛': 'cbrt',
-  MOD: '%',
-  '\\^': '**',
-  '÷': '/',
-  '×': '*'
-};
-
-// dictionary for corresponding inversed function for each functions
-const inverseDict = {
-  sin: 'asin',
-  cos: 'acos',
-  tan: 'atan',
-  deg: 'deg'
-};
-
-// dictionary to store the formatting options (opened)
-const formatDict = {
-  number: '<',
-  operator: '[',
-  function: '{',
-  none: ''
-};
-
-// dictionary to store the formatting options (closed)
-const formatDictClosing = {
-  number: '>',
-  operator: ']',
-  function: '}',
-  none: ''
-};
-
-// dictionary containing corresponding HTML codes of formatted codes
-const htmlCode = {
-  '<': "<input onchange='changeTemp()' type='number' value='",
-  '>': "'>",
-  '\\[': `
-    <select onchange='changeTemp()'>
-      <option>`,
-  '\\]': `</option>
-    <option>+</option>
-    <option>-</option>
-    <option>×</option>
-    <option>÷</option>
-    <option>^</option>
-    </select>
-  `,
-  '\\{': `<select onchange='changeTemp()'><option>`,
-  '\\}': `</option>
-  <option>sin</option>
-  <option>cos</option>
-  <option>tan</option>
-  <option>asin</option>
-  <option>acos</option>
-  <option>atan</option>
-  <option>fact</option>
-  <option>log10</option>
-  <option>abs</option>
-  <option>√</option>
-  <option>∛</option>
-  </select>
-  `,
-  '⟨': '<span>(</span>',
-  '⟩': '<span>)</span>',
-  'π': PI
-
-};
 
 window.onload = () => {
   input = document.querySelector('#input-panel');
@@ -107,7 +32,7 @@ window.onload = () => {
             exp.innerText = temp;
           }
         }
-      // Checks if a function is associated with the button (like equals, sqrt, etc.)
+        // Checks if a function is associated with the button (like equals, sqrt, etc.)
       } else if (x.name === 'func') {
         switch (x.value) {
           case '=':
@@ -200,7 +125,7 @@ window.onload = () => {
         x.style.backgroundColor = 'rgb(144, 223, 228)';
         x.innerHTML = inverseDict[x.value];
         x.value = inverseDict[x.value];
-      // format the button for the normal version
+        // format the button for the normal version
       } else {
         document.getElementById('inverse').style.backgroundColor = 'white';
         x.style.backgroundColor = 'white';
@@ -213,172 +138,13 @@ window.onload = () => {
       }
     });
   };
+  // handler for keydown events
+  window.addEventListener('keydown', (e)=> {
+    document.querySelector("button[value='" + e.key.replace('Enter', '=').replace('/', '÷').replace('*', '×') + "']") ? document.querySelector("button[value='" + e.key.replace('Enter', '=').replace('/', '÷').replace('*', '×') + "']").click() :  false;
+  });
 };
-
-// Checks if the last value in the math input panel is an operator
-function lastIsOperator () {
-  return input.value === '' && (/(\+|-|\/|\*|\^|÷|×)/.test(temp.charAt(temp.length - 1)) || temp.endsWith('MOD '));
-}
-
-// Checks if the decimal has been used in the input panel
-function hasDecimal () {
-  let expressions = input.value.split(/(\+|-|\/|\*|\^|MOD)/g);
-  return /\./g.test(expressions.pop());
-}
-
-// Adds mathematical functions to the expressions
-function addFunction (func) {
-  if (input.value !== '') {
-    input.value = func + '(' + input.value + ')';
-  }
-}
-
-// Handles braces according to the entered values in input
-function handleBraces () {
-  /* Braces are allowed to open if,
-      * there are no expressions in the input or exp panel yet
-      * the last character in exp panel is an arithmetic symbol
-      * the last character seen is not a number or a point
-  */
-
-  /* Bracese should close if,
-      * last character is a number or an opening or closing bracket
-  */
-  if (temp === '' && input.value === '') {
-    addBracket();
-  } else if (input.value === '' && (/(\+|-|÷|×|\^)/.test(temp.charAt(temp.length - 1)) || temp.endsWith('MOD '))) {
-    addBracket();
-  } else if (openedBraces !== 0 && /\d/.test(input.value.charAt(input.value.length - 1))) {
-    closeBracket();
-  } else if (input.value.endsWith('(') || temp.endsWith('(')) {
-    addBracket();
-  } else if ((input.value.endsWith(')') || temp.endsWith(')')) && openedBraces !== 0) {
-    closeBracket();
-  }
-  console.log(openedBraces);
-}
-
-// Method for appending brackets
-function addBracket () {
-  input.value += '(';
-  openedBraces++;
-  responsiveVoice.speak('open bracket', 'US English Female');
-}
-
-// Method for closing brackets
-function closeBracket () {
-  input.value += ')';
-  openedBraces--;
-  responsiveVoice.speak('close bracket', 'US English Female');
-}
-
-// Calculates the factorial in the most famous one - liner method.
-const fact = n => n < 2 ? 1 : fact(n - 1) * n;
-
-// Gets the result of the expression
-function getResult (expression) {
-  let ans;
-  let f = expression;
-  for (let [k, v] of Object.entries(sym)) {
-    f = f.replace(new RegExp(k, 'gi'), v);
-  }
-  try {
-    exp.innerHTML = '';
-    ans = eval(f);
-    return ans;
-  } catch (e) {
-    console.error('Error: ' + e.message);
-    responsiveVoice.speak('OOPS! An error occured', 'US English Female');
-    return expression;
-  } finally {
-    if (typeof ans !== 'undefined') {
-      // check if the no history message box exists and if yes remove it, otherwise silently ignore it.
-      document.querySelector('#no_hist_msg') ? document.querySelector('#no_hist_msg').remove() : false;
-      hist.innerHTML = '<var>' + expression + '</var><br /><b>' + ans + '</b><br /><hr />' + hist.innerHTML;
-      memory.push(ans);
-      responsiveVoice.speak('Equals ' + ans, 'US English Female');
-
-      console.log(memory);
-    }
-  }
-}
 
 // Handler for content changes
 function oncontent () {
   exp.innerHTML = formatToHTML(formatExpression(temp));
-}
-
-// Formats expression to a symobl format
-function formatExpression (ex) {
-  /*
-    format reference
-      integers: <n>
-      operators: []
-      functions: {}
-  */
-  let chunks = ex.replace(/\(/g, '⟨').replace(/\)/g, '⟩').split('');
-  let lastElemType = getType(chunks[0]);
-  let formatted = formatDict[lastElemType];
-  // let fragmentClosed = false;
-  for (let c of chunks) {
-    console.log(c + ':  ' + getType(c));
-    if (getType(c) === lastElemType) {
-      formatted += c;
-      // fragmentClosed = false;
-    } else {
-      // TODO: search a way to format brackets -> Line 296 originally doesn't had that ternary operation
-      formatted += formatDictClosing[lastElemType] + formatDict[getType(c)] + c;
-      lastElemType = getType(c);
-    }
-  }
-  return formatted;
-}
-
-function formatToHTML (form) {
-  if (temp === '') {
-    return '';
-  }
-  for (let [k, v] of Object.entries(htmlCode)) {
-    form = form.replace(new RegExp(k, 'g'), v);
-  }
-  return form;
-}
-
-// Returns the mathematical type of the passed element
-function getType (elem) {
-  if (/[\dπ.]/.test(elem)) {
-    return 'number';
-  } else if (/(\+|-|\/|\*|\^|÷|×)/.test(elem)) {
-    return 'operator';
-  } else if (/[\w∛√]/.test(elem)) {
-    return 'function';
-  } else {
-    return 'none';
-  }
-}
-
-// Handles onchange eventes of elements in exp and change the temp accordingly
-function changeTemp () {
-  temp = '';
-  Object.values(exp.children).forEach(x => {
-    // checking if the element contains a bracket (generally span elements store brackets)
-    if (x.nodeName === 'SPAN') {
-      temp += x.innerText;
-    } else {
-      // add the value stored in the element if it is not a span (bracket)
-      temp += x.value;
-    }
-  });
-  return temp;
-}
-
-// function for retriving speech text and speak the text
-function getSpeechText (elem) {
-  if (elem.getAttribute('speak') !== null && elem.getAttribute('speak') !== '!custom') {
-    responsiveVoice.cancel();
-    responsiveVoice.speak(elem.getAttribute('speak'), 'US English Female');
-  } else if (elem.getAttribute('speak') !== null && elem.getAttribute('speak') === '!custom') {
-    responsiveVoice.cancel();
-    // do nothing
-  }
 }
